44d11691fff1c405e51068415c8b3ae05128ec29
Devanand <devanandkangane@gmail.com>
2023-12-27 20:21:10 +0530

    update key

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def get_vectorStore(chunks):
 def get_conversationChain(vectorStore):
     # llm = ChatOpenAI()
     huggingfacehub_api_token = st.secrets['HUGGINGFACEHUB_API_KEY']
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl",huggingfacehub_api_token=huggingfacehub_api_token, model_kwargs={"temperature":0.5, "max_length":512})
     memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
     chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
